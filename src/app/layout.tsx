9f8ddecd6567e9f2a1ab73cf991cf71958a7922f
abhiwaghmare <a1999waghmare@gmail.com>
2024-12-15 04:14:46 +0530

    project setup and navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
         {children}
+        </div>
       </body>
     </html>
   );
