28f76c796a25d960a562831eb5c5bc11aff38d87
abhiwaghmare <a1999waghmare@gmail.com>
2024-12-18 20:44:02 +0530

    Add MeetOurTeam and UpcomingWebinars components; update Home page and Tailwind config; add simplex-noise dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MeetOurTeam from "@/components/MeetOurTeam";
 import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 
@@ -11,6 +13,8 @@ export default function Home() {
         <FeaturedCourses />
         <WhyChooseUs />
         <TestimonialCards />
+        <UpcomingWebinars />
+        <MeetOurTeam />
    </div>
   );
 }
